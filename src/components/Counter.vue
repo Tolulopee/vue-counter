0d8bfa89da35ed1b077f745b9bf48e1e5976a7b0
Kalejaiye Tolulope <tolulopekay56@gmail.com>
2023-03-06 12:16:29 -0800

    started

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button  @click="increment">Increment</button>
+        <button @click="decrement" >Decrement</button>
+        <button @click="setvalue">SetValue</button>
+        <button @click="reset">Reset</button>
+        <p>{{ count }}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Counter',
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        
+    }
+}
+
+</script>
+
+<style scoped>
+</style>
